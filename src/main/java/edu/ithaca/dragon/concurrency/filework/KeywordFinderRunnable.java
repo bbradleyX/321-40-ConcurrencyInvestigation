90af20cfeac21520c36aeff0ed6965cc0c6c3754
bbradleyX <bbradley@ithaca.edu>
2020-11-19 13:17:31 -0500

    updates

--- src/main/java/edu/ithaca/dragon/concurrency/filework/KeywordFinderRunnable.java
+++ src/main/java/edu/ithaca/dragon/concurrency/filework/KeywordFinderRunnable.java
@@ -7,6 +7,9 @@ public class KeywordFinderRunnable implements Runnable {
     int index;
     String filename;
     int amountOfPretendWork;
+    /* boolean testingForBoth;
+    String workType1;
+    String workType2; */
 
     /**public KeywordFinderRunnable(int[] array, int index, String filename) {
         this.array = array;
@@ -22,27 +25,49 @@ public class KeywordFinderRunnable implements Runnable {
     }
 
     public void run() {
-        readDocument(amountOfPretendWork);
+        //readDocument(amountOfPretendWork);
         processDocument(amountOfPretendWork);
         array[index] = new Random().nextInt(1000);
     }
 
-    private void readDocument(int amount) {
+    /* private void readDocument(int amount) {
         try {
             //fake IO work
+            //this.workType1 = "IO";
             Thread.sleep(amount);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-    }
+    } */
 
-    private void processDocument(int amount){
+     private void processDocument(int amount){
         //fake CPU work
+        //this.workType1 = "CPU";
         double x = 1000000;
 		for (int y=0; y<amount*1000; y++){
 			x/=2;
 		}
-    }
+    } 
+
+    /* private void workType(){
+        if(this.workType2 == "both"){
+            this.testingForBoth = true;
+            System.out.println(testingForBoth);
+        }
+        else if (this.workType1 == "CPU"){
+            this.testingForBoth = false;
+            System.out.println(testingForBoth);
+        }
+        else if (this.workType1 == "IO"){
+            this.testingForBoth = false;
+            System.out.println(testingForBoth);
+        }
+        else{
+            this.testingForBoth = false;
+            System.out.println(testingForBoth);
+        }
+
+    } */
 
     
 
